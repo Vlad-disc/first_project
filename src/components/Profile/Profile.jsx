e86f681e517d4d20c5d109ce851294bc0b230b76
Uskov <uskov_vladislav@icloud.com>
2022-01-18 15:45:25 +0300

    lessons 20 - NavLink

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,15 +3,17 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div>
+  return (
     <div>
-      <img src='https://images.unsplash.com/photo-1568890724546-a0ad828fd94d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80'></img>
+      <div className={s.container}>
+        <img src='https://images.unsplash.com/photo-1568890724546-a0ad828fd94d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80'></img>
+      </div>
+      <div className={s.item}>
+        ava+description
+      </div>
+      <MyPosts />
     </div>
-    <div>
-      ava+description
-    </div>
-    <MyPosts />
-  </div>
+  ) 
 }
 
 export default Profile;
